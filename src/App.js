cc66fbac3d7ef140db0d1fc2e7634d038f8399ba
ssoheily <sam.soheil55@gmail.com>
2022-06-24 19:14:03 +0200

    first ´commit(product.js,css,app.js,css ready)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react"
+import "./App.css"
+import { Product } from "./products/Product"
+ 
+/*1. creste component   */
+export default function App() {
+  let allProduct=[
+    {id:1 , title:"Chicken" ,image:"images/gallery_1.jpeg", price:40 ,count:4},
+    {id:2 , title:"Pommes" ,image:"images/gallery_2.jpeg", price:13 ,count:23},
+    {id:3 , title:"Hamburger" ,image:"images/gallery_3.jpeg", price:15 ,count:31},
+  ]
+  return ( 
+    <div className="container">
+    <Product {...allProduct[0]}> <p>smartphoene description is ready to sale 2022,.... </p></Product>
+    <Product {...allProduct[1]}> <p>smartphoene description is ready to sale 2022,.... </p></Product>
+    <Product {...allProduct[2]}> <p>smartphoene description is ready to sale 2022,.... </p></Product>
     </div>
-  );
+    
+  )
 }
 
-export default App;
+ 
---
+++ src/products/Product.js
@@ -0,0 +1,43 @@
+import "./Product.css";
+export function Product({ title:productTitle, image, price, count,children }) {
+  // console.log(props);
+    let title= "product card"
+  return (
+    count && (
+      <div>
+        <h2>{title}</h2>
+        <div className="card">
+          <img src={image} alt="productPic" />
+          <h1>{productTitle}</h1>
+          <p className="price">${price}</p>
+            {children}
+         
+          <p>
+            <button>Add to Cart</button>
+          </p>
+          <p></p>
+        </div>
+      </div>
+    )
+  );
+}
+
+Product.defaultProps ={
+    image:"images/javascript.jpg",price:"0",title:"test"
+}
+/* (
+      <div>
+        <h2>{title}</h2>
+        <div className="card">
+          <img src={image} alt="productPic" />
+          <h1>{productTitle}</h1>images/javascript.jpg
+          <p className="price">${price}</p>
+          <p>smartphoene description is ready to sale 2022,.... </p>
+          {children}
+          <p>
+            <button>Add to Cart</button>
+          </p>
+          <p></p>
+        </div>
+      </div>
+    ) */
