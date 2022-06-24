f5494a3bd2a77bd37add318fe5c4548352ee9b44
ssoheily <sam.soheil55@gmail.com>
2022-06-24 19:57:26 +0200

    create navbar header

---
+++ src/header/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+export default function NavBar() {
+  return (
+    <header>
+      <h1>MynavbarSite</h1>
+      <nav>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Services</a>
+      </nav>
+    </header>
+  );
+}
