bde948ed0f06f7bb0c7f73faa184a34d718b75d3
ssoheily <sam.soheil55@gmail.com>
2022-06-26 13:05:01 +0200

    first commit for Sam.soheil-project-componenet-base

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 
 import React from "react"
-import "./App.css"
-import { Product } from "./products/Product"
-import Header from "./header/Header"
+
+
  
 /*1. creste component   */
-export default function App() {
-  let allProduct=[
-    {id:1 , title:"Chicken" ,image:"images/gallery_1.jpeg", price:40 ,count:4},
-    {id:2 , title:"Pommes" ,image:"images/gallery_2.jpeg", price:13 ,count:23},
-    {id:3 , title:"Hamburger" ,image:"images/gallery_3.jpeg", price:15 ,count:31},
-  ]
-  return ( 
-    <div>
-      <Header></Header>
-      <div className="container">
-          <Product {...allProduct[0]}> <p>smartphoene description is ready to sale 2022,.... </p></Product>
-          <Product {...allProduct[1]}> <p>smartphoene description is ready to sale 2022,.... </p></Product>
-          <Product {...allProduct[2]}> <p>smartphoene description is ready to sale 2022,.... </p></Product>
-      </div>
-    </div>
- 
-    
-  )
+export default class App extends React.Component {
+  render(){
+    let allProduct=[
+      {id:1 , title:"Chicken" ,image:"images/gallery_1.jpeg", price:40 ,count:4},
+      {id:2 , title:"Pommes" ,image:"images/gallery_2.jpeg", price:13 ,count:23},
+      {id:3 , title:"Hamburger" ,image:"images/gallery_3.jpeg", price:15 ,count:31},
+    ]
+    return ( 
+      <div>
+
+        <div className="container">
+       
+        </div>
+      </div>  
+    )
+  }
+
 }
 
  
